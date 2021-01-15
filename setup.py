28d915dc0cefbfeda2ebc8757b5a8e7b8f286c71
thanasisDr <thanasis.dritsas@gmail.com>
2021-01-15 14:17:06 +0100

    delete

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(name='edaViz4ml',
-      version='0.1',
-      description='Methods for EDA and ML models visualization',
-      packages=['edaViz4ml'],
-      zip_safe=False)
