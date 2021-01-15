2d6e51679b32ac40d39ba8ce52dc7dae9b22b77d
thanasisDr <thanasis.dritsas@gmail.com>
2021-01-15 14:22:05 +0100

    Only EDA

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='edaViz4ml',
+      version='0.1',
+      description='Methods for EDA and ML models visualization',
+      packages=['edaViz4ml'],
+      zip_safe=False)
