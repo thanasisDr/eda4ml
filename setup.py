decdf342a588f68d52580a7bb99cef3d261dc296
thanasisDr <thanasis.dritsas@gmail.com>
2021-01-17 15:58:15 +0100

    PyPi package ready

--- setup.py
+++ setup.py
@@ -4,4 +4,6 @@ setup(name='eda4ml',
       version='0.1',
       description='Methods for EDA in the ML process',
       packages=['eda4ml'],
+      author='Athanasios Dritsas',
+      author_email='thanasis.dritsas@gmail.com',
       zip_safe=False)
